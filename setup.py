f1178a49605573ccfcb8bf67fe0868defee02d9c
Himanshu <hbansal_be21@thapar.edu>
2024-01-23 23:38:28 +0530

    New version created

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Implementation of Topsis'
 
 setup(
